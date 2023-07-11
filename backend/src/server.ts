ddb30cfe126e74f4e43479c366b543ffad235491
athenacats <124017569+athenacats@users.noreply.github.com>
2023-07-11 12:50:54 +0300

    Create server.ts and add initial code

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:4200/"],
+  })
+);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Server is running on http://localhost:" + port);
+});
