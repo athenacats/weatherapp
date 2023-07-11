4716fde5d4e6ed58772d423b3a2a6b8ec04148d4
athenacats <124017569+athenacats@users.noreply.github.com>
2023-07-11 15:02:50 +0300

    Edit weather service http request

--- frontend/src/app/services/weather.service.ts
+++ frontend/src/app/services/weather.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { SEARCH_RESULT } from '../shared/constants/urls';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private http: HttpClient) {}
+
+  getWeather(searchTerm: string) {
+    return this.http.get<any>(SEARCH_RESULT + '?city=' + searchTerm);
+  }
 }
