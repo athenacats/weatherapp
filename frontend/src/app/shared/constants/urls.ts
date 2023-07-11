1e108019bcdec0acf8d00d3cf7a2fea3bbdb6a9a
athenacats <124017569+athenacats@users.noreply.github.com>
2023-07-11 15:03:01 +0300

    Correct the urls for search

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:4000/';
+const BASE_URL = 'http://localhost:4000';
 
 export const HOME_URL = BASE_URL + '/api';
-export const SEARCH_RESULT = HOME_URL + '/search/';
+export const SEARCH_RESULT = HOME_URL + '/weather';
