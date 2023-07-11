915a1bc59ca7856dc24b6638b0990714c1c43b39
athenacats <124017569+athenacats@users.noreply.github.com>
2023-07-11 16:02:45 +0300

    Set up default location as Nairobi, home.ts code

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  searchTerm = '';
+  constructor(private weatherService: WeatherService) {}
 
+  ngOnInit(): void {
+    this.weatherService.getWeather('nairobi').subscribe(
+      (weatherData) => {
+        console.log(weatherData); // Display the weather data in the browser console for now
+      },
+      (error) => {
+        console.error(error); // Handle the error appropriately
+      }
+    );
+  }
+  search(term: string): void {
+    this.searchTerm = term; // this line enables the search term be used in backend
+    this.weatherService.getWeather(this.searchTerm).subscribe(
+      (weatherData) => {
+        console.log(weatherData); // Display the weather data in the browser console for now
+      },
+      (error) => {
+        console.error(error); // Handle the error appropriately
+      }
+    );
+  }
 }
