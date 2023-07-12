c60db0bc905631174a233a095afb9af5e2103376
athenacats <124017569+athenacats@users.noreply.github.com>
2023-07-12 10:11:25 +0300

    Fix access to observable data

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, catchError, tap, throwError } from 'rxjs';
 import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
@@ -15,39 +15,29 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.weatherData$ = this.weatherService.getWeather('nairobi').pipe(
+      tap((weatherData) => {
+        console.log(weatherData); // Display the weather data in the browser console for now
+        console.log(weatherData.current.condition.text);
+      }),
       catchError((error) => {
         this.errorMessage = 'An error occurred while fetching weather data.';
         console.error(error);
         return throwError(() => new Error(error));
       })
     );
-    this.weatherData$.subscribe({
-      next: () => {
-        console.log(this.weatherData$), // Display the weather data in the browser console for now
-          console.log(this.weatherData$.current.condition.text);
-      },
-      error: (error) => {
-        console.error(error);
-      },
-    });
   }
   search(term: string): void {
     this.searchTerm = term; // this line enables the search term be used in backend
     this.weatherData$ = this.weatherService.getWeather(this.searchTerm).pipe(
+      tap((weatherData) => {
+        console.log(weatherData); // Display the weather data in the browser console for now
+        console.log(weatherData.current.condition.text);
+      }),
       catchError((error) => {
         this.errorMessage = 'An error occurred while fetching weather data.';
         console.error(error);
         return throwError(() => new Error(error));
       })
     );
-    this.weatherData$.subscribe({
-      next: () => {
-        console.log(this.weatherData$), // Display the weather data in the browser console for now
-          console.log(this.weatherData$.current.condition.text);
-      },
-      error: (error) => {
-        console.error(error);
-      },
-    });
   }
 }
