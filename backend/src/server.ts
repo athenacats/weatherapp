bd4da4fb2b5ccab450e6774835f89781da4048aa
athenacats <124017569+athenacats@users.noreply.github.com>
2023-07-13 17:13:22 +0300

    Prepare for production

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import weatherRouter from "./routers/weather.router";
 import dotenv from "dotenv";
+import path from "path";
 dotenv.config();
 
 const app = express();
@@ -14,6 +15,11 @@ app.use(
 );
 
 app.use("/api", weatherRouter);
+
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,4 +1,6 @@
-const BASE_URL = 'http://localhost:4000';
+import { environment } from 'src/environments/environment';
+
+const BASE_URL = environment.production ? '' : 'http://localhost:4000';
 
 export const HOME_URL = BASE_URL + '/api';
 export const SEARCH_RESULT = HOME_URL + '/weather?city=';
